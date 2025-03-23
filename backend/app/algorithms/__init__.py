ca5be7f9a324777602d7a1e582ab43e1136a73ea
chloefeal <188809157+chloefeal@users.noreply.github.com>
2025-03-23 18:14:16 +0800

    feat: add belt_broken_series

--- backend/app/algorithms/__init__.py
+++ backend/app/algorithms/__init__.py
@@ -3,11 +3,13 @@ from .object_detection import ObjectDetectionAlgorithm
 from .template_algorithm import TemplateAlgorithm
 from .belt_broken import BeltBroken
 from .belt_deviation_detection import BeltDeviationDetection
+from .belt_broken_series import BeltBrokenSeries
 
 __all__ = [
     'BaseAlgorithm',
     'ObjectDetectionAlgorithm',
     'TemplateAlgorithm',
     'BeltBroken',
-    'BeltDeviationDetection'
+    'BeltDeviationDetection',
+    'BeltBrokenSeries'
 ]
